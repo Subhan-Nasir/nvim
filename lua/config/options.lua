a1cfed94b893330dfc0e766cb3a1ac447e5c832c
Subhan-Nasir <i40210@verisk.com>
2025-06-24 14:45:09 +0100

    Copy lsp config from kickstart.nvim

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -23,3 +23,12 @@ vim.diagnostic.config({
 })
 
 vim.o.sessionoptions="blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+    group = vim.api.nvim_create_augroup('highlight_yank', {}),
+    desc = 'Hightlight selection on yank',
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank { higroup = 'IncSearch', timeout = 300 }
+    end,
+})
