8b4ac088402ebc107277abe3bd3b7100b066347a
Subhan-Nasir <i40210@verisk.com>
2026-03-30 16:22:37 +0100

    Update blink cmp config

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -7,9 +7,10 @@ return {
             preset = "enter",
             ["<CR>"] = { "accept", "fallback" },
             ["<Tab>"] = { "accept", "fallback" },
-            ["<C><leader>"] = { "show" },
             ["<a-j>"] = { "select_next", "fallback" },
             ["<a-k>"] = { "select_prev", "fallback" },
+            ["<C-Space>"] = { "show" },
+            ["<C-e>"] = { "hide" },
         },
         -- highlight = {
         --     use_nvim_cmp_as_default = true,
@@ -18,22 +19,39 @@ return {
             nerd_font_variant = 'mono'
         },
         completion = {
+            ghost_text = { enabled = false },
+            -- Only auto-select when there is one unambiguous match
+            list = { selection = { preselect = true, auto_insert = false } },
+            trigger = { show_on_keyword = true, show_on_trigger_character = true },
+            menu = {
+                auto_show = true,
+            },
             documentation = {
                 auto_show = true,
+                auto_show_delay_ms = 500,
             },
         },
         sources = {
-            default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+            default = { "lazydev", "lsp", "path", "snippets" },
             providers = {
                 lazydev = {
                     name = "LazyDev",
                     module = "lazydev.integrations.blink",
-                    -- make lazydev completions top priority (see `:h blink.cmp`)
                     score_offset = 100,
+                },
+                lsp = {
+                    score_offset = 50,
+                    min_keyword_length = 2,
+                },
+                snippets = {
+                    min_keyword_length = 3,
+                    score_offset = -10,
+                },
+                path = {
+                    min_keyword_length = 2,
                 },
             },
         },
-
         fuzzy = { implementation = "prefer_rust_with_warning" }
     },
     opts_extend = { "sources.default" }
